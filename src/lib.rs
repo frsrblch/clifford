84b156fa73ae4fbd9dbaaea42532caceae996b19
Fraser Balch <farseer.ulwithy@gmail.com>
2026-04-19 14:38:35 -0600

    export num_traits::{one, zero}

--- src/lib.rs
+++ src/lib.rs
@@ -116,4 +116,4 @@ pub use geo_traits::{
     ZeroConst,
 };
 pub use macros::{algebra, lean_algebra};
-pub use num_traits::{FloatConst, Inv, One, Zero};
+pub use num_traits::{one, zero, FloatConst, Inv, One, Zero};
